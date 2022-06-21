4fb07ad47da69794654994f18599758e5118204c
JonAnderUni <jmiguel011@ikasle.ehu.eus>
2022-06-21 16:24:30 +0200

    Ejercicio Terminado

--- prueba001.py
+++ prueba001.py
@@ -1,23 +1,44 @@
 import json
 from urllib.request import urlopen
+import csv
 url = "https://labsland.com/api/labs?&country=ES&lang=es"
-response = urlopen(url)
-datos = json.loads(response.read())
-#if(key == 'educationLevels'):      NECESITO COGER SOLO EL DE LA UNI, LOS DEMAS A TOMAR POR CULO, CAMBIAR VARIABLES DE LOS FOR
-          #  print(value[1])
-print(type(datos))
-for cosita in datos.values():
-    for veremos in cosita:
-        print(type(veremos))
-        for key, value in veremos.items():
-           if(key == 'educationLevels'):
-               for prueba in value:
-                   if(prueba == 'university'):
-                       print("SOY UNIVERSITARIO, QUE LISTO SOY YO")
-                   elif(prueba == 'ms'):
-                       print("Estoy en la ESO")
-                   elif(prueba == 'hs'):
-                       print("Llegue a bachiller")
-                   elif(prueba == 'elementary'):
-                       print("Tengo miedo de ir al cole")
+respuesta = urlopen(url)
+datosJSON = json.loads(respuesta.read())
+#with open('labs.json') as archivo:
+  #datosJSON = json.load(archivo)
+
+data = ['Nombre Laboratorio', 'Descripcion Laboratorio', 'Institucion Laboratorio']
+#      NECESITO COGER SOLO EL DE LA UNI, LOS DEMAS A TOMAR POR CULO, CAMBIAR VARIABLES DE LOS FOR
+with open('laboratoriosUniversidad.csv','w') as file:
+    write = csv.writer(file)
+    write.writerow(data)
+nombreLab = ''
+descrLab = ''
+institLab= ''
+
+
+for dic_todo in datosJSON.values():
+    for dic in dic_todo:
+
+        paraImprimir = True
+        for key, value in dic.items():
+
+            if(key == 'institution'):
+                institLab = value
+            elif(key == 'description'):
+                descrLab = value
+            elif(key == 'name'):
+                nombreLab = value
+            elif(key == 'educationLevels'):
+                for prueba in value:
+                    if(prueba == 'hs' or prueba == 'ms'):
+                        paraImprimir = False
+
+
+        if (paraImprimir == True):
+            lista = [nombreLab, descrLab, institLab]
+            print(lista)
+            with open('laboratoriosUniversidad.csv', 'a') as file:
+                writer = csv.writer(file)
+                writer.writerow(lista)
 
